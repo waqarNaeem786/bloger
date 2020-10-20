fb981a2f17825549270fb0a2973b3666f0023324
Waqar <neutrone04@gmail.com>
2020-10-20 20:50:47 +0500

    main

--- model/main.js
+++ model/main.js
@@ -1,7 +1,17 @@
 /*submit function*/
 
-    const post_form = document.querySelectorAll('#post_form');
-    
+    const post_form = document.getElementById('post_form');
+    post_form.addEventListener('submit',(e) => {
+        e.preventDefault;
+        if(post_form.field.value != ''){
+            console.log('not empty')
+        }then(result => {
+            console.log(result)        
+        }).catch((err) => {
+            console.log(err)
+        });
+
+    })
     
 
     
